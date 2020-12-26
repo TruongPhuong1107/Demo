78b1a96a04418c931e22c21bc5a5aba6014586d8
Truong Phuong <truongphuongvt1107@gmail.com>
2020-12-26 11:03:47 +0700

    kjkk

---
+++ src/Components/Create.js
@@ -0,0 +1,30 @@
+import React, { Component} from "react";
+export default class Create extends Component{
+    render(){
+        return(
+            <div className="create-container">
+                <h3>
+                    Thêm mới mặt hàng
+                </h3>
+                <form> 
+                    <div  className="form-group">  
+                      <label>Tên mặt hàng:</label>
+                      <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                      <label>Loại:</label>
+                      <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                      <label>Giá:</label>
+                      <input type="text" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                      <button className="btn btn-danger">Thêm</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+//export default Create;
